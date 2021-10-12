9f959423afc61bc4b4db62b1a003442a8b13defa
TAYLOR CALLAN WILLIAMS <taylor.callan.williams@gmail.com>
2021-10-12 15:13:23 -0700

    signup page drafted

--- src/signUp.js
+++ src/signUp.js
@@ -1,10 +1,38 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { signUp } from './fetchUtils';
 
 export default class signUp extends Component {
+
+    state = {
+        password: '',
+        email: '',
+    }
+
+    handleSubmit = async e => {
+        e.preventDefault();
+        const { token } = await signUp(this.state.email, this.state.password);
+        this.props.handleTokenChange(token);
+        this.props.history.push('/todos');
+    }
+
     render() {
         return (
             <div>
-                BINGUS
+                <form onSubmit = {this.handleSubmit}>
+                    <label>
+                        Email Address:
+                        <input value = {this.state.email} onChange = { (e) => this.setState ({ email : e.target.value})} type = "email" />
+                    </label>
+                    <label>
+                        Password:
+                        <input value = {this.state.password} onChange = { (e) => this.setState ({ password: e.target.value})} type = "password"/>
+                    </label>
+                    <button>Sign Up!</button>
+                </form>
+
+                <Link to="/login"> Log in to existing account </Link>
+        
             </div>
         )
     }
